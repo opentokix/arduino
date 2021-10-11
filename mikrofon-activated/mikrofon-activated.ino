c48acd7cd87c34fc2e90bda5901081b5bad88bd8
OpenTokix <petereriksson.net@gmail.com>
2021-10-11 23:25:57 +0200

    Stuff

---
+++ mikrofon-activated/mikrofon-activated.ino
@@ -0,0 +1,36 @@
+
+//Params 
+const int micPin = A0; 
+int micVal = 0; 
+const int relayPin = 4; 
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("Initialize")); 
+  pinMode(micPin, INPUT);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
+  clickTwice();
+}
+
+void clickTwice() {
+  digitalWrite(relayPin, HIGH); 
+  delay(1000);
+  digitalWrite(relayPin, LOW); 
+  delay(300);
+  digitalWrite(relayPin, HIGH); 
+  delay(1000);
+  digitalWrite(relayPin, LOW); 
+}
+
+void readMicrophone() {
+  micVal = analogRead(micPin); 
+  if (micVal > 300) {
+    clickTwice();
+  }
+}
+
+void loop() {
+  readMicrophone();
+}
